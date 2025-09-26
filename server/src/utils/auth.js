f351863f16fdb094abce8c0d01dc67945a171200
MsrSahil <mohammadswahil021@gmail.com>
2025-09-26 23:26:40 +0530

    secure site changes

--- server/src/utils/auth.js
+++ server/src/utils/auth.js
@@ -7,7 +7,7 @@ const genToken = (user, res) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
+    secure: true,
     maxAge: 24 * 60 * 60 * 1000, // 1 day
     sameSite: "lax",
   });
