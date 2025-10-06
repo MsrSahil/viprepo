5210086080609aee51c4585cb3028d3737f7dc3b
MsrSahil <mohammadswahil021@gmail.com>
2025-10-06 16:27:57 +0530

    fix: set secure cookie flag to false for token generation

--- server/src/utils/auth.js
+++ server/src/utils/auth.js
@@ -5,9 +5,9 @@ const genToken = (user, res) => {
     expiresIn: "1d",
   });
 
-    res.cookie("token", token, {
+  res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "Production",
+    secure: false,
     maxAge: 24 * 60 * 60 * 1000, // 1 day
     sameSite: "lax",
   });
