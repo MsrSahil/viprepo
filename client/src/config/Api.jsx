ef1e04c22dc258c561024714efc3cf2663d290cc
MsrSahil <mohammadswahil021@gmail.com>
2025-10-06 15:36:58 +0530

    final

--- client/src/config/Api.jsx
+++ client/src/config/Api.jsx
@@ -1,9 +1,12 @@
 import axios from "axios";
-
+import { cookieStore } from "cookie-store";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
+   headers: {
+    Authorization: `Bearer ${cookieStore.get("token")?.value || ""}`,
+  },
 });
 
 export default api;
